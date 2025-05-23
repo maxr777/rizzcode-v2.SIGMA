327ba75e5f7720190ed3f1d2c8ae019fdbd72449
max <maxr.0307@gmail.com>
2025-05-24 00:33:29 +0200

    Open up the browser on app.py launch

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_wtf import CSRFProtect
+import webbrowser
+import threading
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -73,7 +75,11 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
+def open_browser():
+    webbrowser.open_new("http://localhost:5000/login")
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+    threading.Timer(1.0, open_browser).start()
     app.run(debug=True)
